0edd41c8e2866c81302854c7733a373a5060cec4
Francis Kershaw <73906024+franciskershaw@users.noreply.github.com>
2023-05-27 13:14:07 +0100

    add in correct helper file

--- api/helper/helper.js
+++ api/helper/helper.js
@@ -1,4 +1,47 @@
+const Recipe = require('../models/Recipe');
+
 const jwt = require('jsonwebtoken');
+
+const generateShoppingList = async (menu) => {
+  let shoppingList = [];
+  if (menu.length) {
+    for (let object of menu) {
+      const recipe = await Recipe.findById(object._id).select({
+        ingredients: 1,
+      });
+      const ingredients = recipe.toObject().ingredients;
+      let ingredientsFormated = ingredients.map((ingredient) => {
+        return {
+          ...ingredient,
+          quantity: ingredient.quantity * object.serves,
+          obtained: false,
+        };
+      });
+      shoppingList = [
+        ...shoppingList.filter(
+          (obj) =>
+            !ingredientsFormated.some(
+              (newObj) => newObj._id.equals(obj._id) && newObj.unit === obj.unit
+            )
+        ),
+        ...ingredientsFormated.map((obj) => {
+          const originalObj = shoppingList.find(
+            (originalObj) =>
+              originalObj._id.equals(obj._id) && originalObj.unit === obj.unit
+          );
+          if (originalObj) {
+            return {
+              ...obj,
+              quantity: originalObj.quantity + obj.quantity,
+            };
+          }
+          return obj;
+        }),
+      ];
+    }
+  }
+  return shoppingList;
+};
 
 const generateAccessToken = (id) => {
   return jwt.sign({ _id: id }, process.env.ACCESS_TOKEN_SECRET, {
@@ -17,6 +60,7 @@ const verifyToken = (token, secret) => {
 };
 
 module.exports = {
+  generateShoppingList,
   generateAccessToken,
   generateRefreshToken,
   verifyToken,
